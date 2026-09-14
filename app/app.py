a7180d419c2732991d6aef0be1ae3b7b862416a5
shasankanneboina4-prog <shasankanneboina4@gmail.com>
2026-09-15 03:20:07 +0530

    Dockerize flask operations

--- app/app.py
+++ app/app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/health")
 def health():
-	return {"status": "healthy"}, 200
+    return {"status": "healthy"}, 200
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
